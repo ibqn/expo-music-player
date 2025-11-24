d530d7586c75acea8136dacff5a0a7422076ac7d
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-11-24 10:45:49 +0100

    feat: enhance tab navigation with screen options for better UI

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,8 +1,18 @@
+import { colors, fontSizes } from "@/constants/tokens"
 import { Tabs } from "expo-router"
 
 export default function TabsLayout() {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: colors.primary,
+        tabBarLabelStyle: {
+          fontSize: fontSizes.xs,
+          fontWeight: 500,
+        },
+        headerShown: false,
+      }}
+    >
       <Tabs.Screen name="favorites" />
       <Tabs.Screen name="playlists" />
       <Tabs.Screen name="(songs)" />
