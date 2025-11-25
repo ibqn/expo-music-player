29bb852979af3fce805ea5fb154e8ca9949f94eb
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-11-25 08:07:33 +0100

    feat: enhance utilsStyles with additional styles for layout and empty content

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -12,4 +12,32 @@ export const defaultStyles = StyleSheet.create({
   },
 })
 
-export const utilsStyles = StyleSheet.create({})
+export const utilsStyles = StyleSheet.create({
+  centeredRow: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  slider: {
+    height: 7,
+    borderRadius: 16,
+  },
+  ItemSeparator: {
+    borderColor: colors.textMuted,
+    borderWidth: StyleSheet.hairlineWidth,
+    opacity: 0.3,
+  },
+  emptyContentText: {
+    ...defaultStyles.text,
+    color: colors.textMuted,
+    textAlign: "center",
+    marginTop: 20,
+  },
+  emptyContentImage: {
+    width: 200,
+    height: 200,
+    alignSelf: "center",
+    marginTop: 40,
+    opacity: 0.3,
+  },
+})
