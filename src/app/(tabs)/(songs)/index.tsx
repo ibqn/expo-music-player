eb233eae6cd4a3c413fc939b3c95ee02a75530d6
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-11-25 08:07:48 +0100

    feat: update SongsScreen layout with padding adjustments

--- src/app/(tabs)/(songs)/index.tsx
+++ src/app/(tabs)/(songs)/index.tsx
@@ -1,10 +1,11 @@
 import { TracksList } from "@/components/tracks-list"
+import { screenPadding } from "@/constants/tokens"
 import { defaultStyles } from "@/styles"
 import { View } from "react-native"
 
 export default function SongsScreen() {
   return (
-    <View style={defaultStyles.container}>
+    <View style={{ ...defaultStyles.container, paddingTop: 100, paddingHorizontal: screenPadding.horizontal }}>
       <TracksList />
     </View>
   )
